a0e64ea876932066ee962a544d8e691c89db1a41
Cekerel Tsang <3330363494@qq.com>
2020-11-11 14:32:03 +0800

    Solve problems

--- src/680.验证回文字符串-ⅱ.java
+++ src/680.验证回文字符串-ⅱ.java
@@ -64,22 +64,23 @@ class Solution {
     // }
     public boolean validPalindrome(String s) {
         int low = 0, high = s.length() - 1;
+        char [] chars = s.toCharArray();
         while (low < high) {
-            char c1 = s.charAt(low), c2 = s.charAt(high);
+            char c1 = chars[low], c2 = chars[high];
             if (c1 == c2) {
                 low++;
                 high--;
             } else {
                 boolean flag1 = true, flag2 = true;
                 for (int i = low, j = high - 1; i < j; i++, j--) {
-                    char c3 = s.charAt(i), c4 = s.charAt(j);
+                    char c3 = chars[i], c4 = chars[j];
                     if (c3 != c4) {
                         flag1 = false;
                         break;
                     }
                 }
                 for (int i = low + 1, j = high; i < j; i++, j--) {
-                    char c3 = s.charAt(i), c4 = s.charAt(j);
+                    char c3 = chars[i], c4 = chars[j];
                     if (c3 != c4) {
                         flag2 = false;
                         break;
